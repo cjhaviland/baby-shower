83625ed61efe3a93b23a8c3ac5350e3174babdd2
CJ Haviland <cj.haviland@yahoo.com>
2021-02-03 17:05:18 -0500

    Added the carousel for the form to come

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { MainFormComponent } from './main-form/main-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     MainFormComponent
   ],
   imports: [
